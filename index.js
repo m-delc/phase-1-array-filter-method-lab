613fd6495cd11c1c23f3f2b00fe8313c59d6bb88
MichaelDelcambre <delcambrem@gmail.com>
2021-11-24 12:07:25 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+
+// function findMatching(){}
+
+// const findMatching = (drivers) => {
+//     drivers === 'Bobby'
+//     return drivers
+// };
+
+// const findMmatching = (array) => {
+//     const obj = array.filter((record) = record.result === 'Bobby')
+//     return obj ? obj.driver : undefined;
+// }
+
+function findMatching(drivers, name){
+    const match = drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+    return match
+}
+
+function fuzzyMatch(drivers, string){
+    const match = drivers.filter(driver => driver[0].toUpperCase() === string[0].toUpperCase())
+return match
+}
+
+function matchName(drivers, string){
+    const strings = drivers.filter(driver => driver.name.toUpperCase() === string.toUpperCase());
+    return strings
+}
